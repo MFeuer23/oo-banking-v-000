09d284962a1d60c58df360e17f26c0284a07de66
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-23 20:19:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,13 @@ class Transfer
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
+    end
+  end
 
+  def reverse_transfer
+    if status == "complete"
+      @sender.balance += amount
+      @receiver.balance -= amount
     end
   end
 end
