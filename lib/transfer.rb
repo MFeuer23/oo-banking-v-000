e89cfa3d195f03a1adff2c8e677edd0740e73aa4
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-23 20:10:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -7,4 +7,12 @@ class Transfer
     @receiver = receiver
     @amount = amount
   end
+
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      return true
+    else
+      return false
+    end
+  end
 end
