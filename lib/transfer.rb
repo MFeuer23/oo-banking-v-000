01de41d10a334aa6003d7ea509f44e336d2f0d05
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-23 20:17:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? && @status == "pending" && @sender.balance >= amount
+    if self.valid? && @sender.balance >= amount
       @sender.balance -= amount
       @receiver.balance += amount
       @status = "complete"
